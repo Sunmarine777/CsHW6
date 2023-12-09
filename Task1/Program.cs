f188707d9a936e6c73932b01d4cb3ab75e57534b
Sunmarine <Sunmarine777@yandex.ru>
2023-12-09 18:15:31 +0300

    Task1 string created

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 1: Задайте двумерный массив символов (тип char [,]).
+//Создать строку из символов этого массива.
+
+using System.Data;
+
+const int m = 5;
+const int n = 5;
+char[,] chars = new char[m, n];
+Random rand = new Random();
+
+for (int i = 0; i < m; i++)
+{
+      for (int j = 0; j < n; j++)
+      {
+      chars[i,j] = (char)('a' + rand.Next(0,26));
+      Console.Write(chars[i,j]+" ");
+      }
+Console.WriteLine();
+}
+Console.WriteLine("----------------");
+for (int i = 0; i < m; i++)
+{
+      for (int j = 0; j < n; j++)
+      {
+            Console.Write(Convert.ToString(chars[i,j]+" "));
+      }
+}
+Console.ReadKey(true);
