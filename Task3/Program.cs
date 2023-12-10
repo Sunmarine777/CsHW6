cfcfd79f4e39158e1afc34578aa17fae7a4aa13c
Sunmarine <Sunmarine777@yandex.ru>
2023-12-11 00:18:53 +0300

    Task3 used template

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+using System;
+public class Test
+{
+    public static bool Palindrome(string text)
+    {
+       if(string.IsNullOrWhiteSpace(text))
+          return false;
+  
+        text = text.ToLower();
+        for(int i = 0; i < text.Length / 2; i++)
+        {
+          if(text[i] != text[text.Length - 1 - i])
+            return false;
+        }
+        return true;
+    }
+    
+  public static void Main()
+    {
+      Console.WriteLine("Enter some text: ");
+        var text = Console.ReadLine();
+        var check = Palindrome(text);
+
+        Console.WriteLine(text + " is " + (check ? "" : "not ") + "Palindrome.");
+    }
+}
